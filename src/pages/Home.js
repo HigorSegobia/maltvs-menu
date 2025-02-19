7bd599763232c7789af24cb820e9d830461a4431
HigorSegobia <higor.segobia@gmail.com>
2025-02-19 18:34:45 -0300

    fix route logo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,10 @@ function Home() {
   const [beers, setBeers] = useState([]);
 
   useEffect(() => {
-    fetch('/beers.json')
-      .then((response) => response.json())
-      .then((data) => setBeers(data))
-      .catch((error) => console.error('Erro ao carregar as cervejas:', error));
+    fetch('./beers.json')
+  .then((res) => res.json())
+  .then((data) => setBeers(data))
+    .catch((error) => console.error('Erro ao carregar as cervejas:', error));
   }, []);
 
   return (
